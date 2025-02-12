49236e75d141f4e72ed7a2fb51bcb7d3147682ba
Victor Andres Marimon Mendoza <vmarimon1@udi.edu.co>
2025-02-11 20:19:35 -0500

    feat: Project Initialization

---
+++ src/main/java/com/campuslands/Mi_Red_Social/MiRedSocialApplication.java
@@ -0,0 +1,13 @@
+package com.campuslands.Mi_Red_Social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiRedSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiRedSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campuslands/Mi_Red_Social/MiRedSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuslands.Mi_Red_Social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiRedSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
