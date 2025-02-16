853d9a4f73ac0be98cfdf2874fcdb82f6aa5660d
Victor Andres Marimon Mendoza <vmarimon1@udi.edu.co>
2025-02-16 09:58:27 -0500

    feat: New entity that allows login

---
+++ src/main/java/com/campuslands/Mi_Red_Social/auth/LoginRequest.java
@@ -0,0 +1,37 @@
+package com.campuslands.Mi_Red_Social.auth;
+
+public class LoginRequest {
+    String username;
+    String password;
+
+    public LoginRequest(){}
+
+    public LoginRequest(String password, String username) {
+        this.password = password;
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginRequest{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
