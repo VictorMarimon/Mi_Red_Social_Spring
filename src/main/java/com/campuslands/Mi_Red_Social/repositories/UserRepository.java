1ad6b4da96656e878529f27b9c598a3dcc0e67dd
Victor Andres Marimon Mendoza <vmarimon1@udi.edu.co>
2025-02-16 09:57:24 -0500

    feat: New search method added

--- src/main/java/com/campuslands/Mi_Red_Social/repositories/UserRepository.java
+++ src/main/java/com/campuslands/Mi_Red_Social/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import com.campuslands.Mi_Red_Social.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
 }
